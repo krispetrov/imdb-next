2e6f5e7ed2a7a1f6096bc01fcbe98022cc23dd6f
Kristiyan Petrov <kristiyan93@gmail.com>
2025-04-27 21:21:35 +0300

    add use client to page.jsx

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,5 +1,7 @@
+'use client';
 import React from "react";
 import Results from "@/components/Results";
+
 
 export default async function SearchPage({ params }) {
   const searhTerm = params.searchTerm;
